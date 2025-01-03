1720a9a9db1ba1b1f96cc8f6b833a7ab15ada218
packo0 <pdimitrov1389@gmail.com>
2025-01-03 17:25:22 +0200

    verify transactions with all method and list comprehensions

--- blockchain.py
+++ blockchain.py
@@ -121,6 +121,17 @@ def verify_chain():
     return True
 
 
+def verify_transactions():
+    return all([verify_transaction(tx) for tx in open_transactions])
+    # is_valid = true
+    # for tx in open_transactions:
+    #     if verify_transaction(tx):
+    #         is_valid = True
+    #     else:
+    #         is_valid = False
+    # return is_valid
+
+
 waiting_for_input = True
 
 while waiting_for_input:
@@ -129,6 +140,7 @@ while waiting_for_input:
     print("2: Mine a new block")
     print("3: Output the blockchain blocks")
     print("4: Output participants")
+    print("5: Check transaction validity")
     print("h: Manipulate the chain")
     print("q: Quit")
     user_choice = get_user_choice()
@@ -147,6 +159,11 @@ while waiting_for_input:
         print_blockchain_elements()
     elif user_choice == "4":
         print(participants)
+    elif user_choice == "5":
+        if verify_transactions():
+            print("All transactions are valid")
+        else:
+            print("There are invalid transactions")
     elif user_choice == "h":
         if len(blockchain) >= 1:
             blockchain[0] = {
