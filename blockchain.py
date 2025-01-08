40601f2538ff4c4b0726bbbe6cbdfb22b54c02ab
Packo0 <pdimitrov1389@gmail.com>
2025-01-08 09:45:57 +0200

    store the blockchain in file

--- blockchain.py
+++ blockchain.py
@@ -13,6 +13,23 @@ owner = "Sender name"
 participants = {"Sender name"}
 
 
+def load_data():
+    with open("blockchain.txt", mode="r") as f:
+        file_content = f.readlines()
+        global blockchain
+        global open_transactions
+        blockchain = file_content[0]
+        open_transactions = file_content[1]
+
+
+load_data()
+
+
+def save_data():
+    with open("blockchain.txt", mode="w") as f:
+        f.write(str(blockchain))
+        f.write("\n")
+        f.write(str(open_transactions))
 
 
 def valid_proof(transactions, last_hash, proof):
@@ -88,6 +105,7 @@ def add_transaction(recipient, sender=owner, amount=1.0):
         open_transactions.append(transaction)
         participants.add(sender)
         participants.add(recipient)
+        save_data()
         return True
     return False
 
@@ -114,6 +132,7 @@ def mine_block():
         "proof": proof,
     }
     blockchain.append(block)
+    save_data()
     return True
 
 
