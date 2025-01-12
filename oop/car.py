78dc826a432ad4fded6e2aaa518b3f4f4e9d8c0a
packo0 <pdimitrov1389@gmail.com>
2025-01-12 12:24:30 +0200

    constructor and instance attributes

--- oop/car.py
+++ oop/car.py
@@ -1,6 +1,10 @@
 class Car:
-    top_speed = 100
-    warnings = []
+    # top_speed = 100
+    # warnings = []
+
+    def __init__(self, starting_top_speed=100):
+        self.top_speed = starting_top_speed
+        self.warnings = []
 
     def drive(self):
         print("I am driving but certainly not faster than {}".format(self.top_speed))
