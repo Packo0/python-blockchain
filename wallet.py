c985f57d986587c90651b7bef0263e30c6bc4d00
Packo0 <pdimitrov1389@gmail.com>
2025-01-20 14:35:08 +0200

    create transaction signature

--- wallet.py
+++ wallet.py
@@ -1,7 +1,9 @@
 from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA256
 import Crypto.Random
-import binascii
 
+import binascii
 
 class Wallet:
     def __init__(self):
@@ -41,3 +43,9 @@ class Wallet:
             binascii.hexlify(private_key.exportKey(format="DER")).decode("ascii"),
             binascii.hexlify(public_key.exportKey(format="DER")).decode("ascii"),
         )
+        
+    def sign_transaction(self, sender, recipient, amount):
+        signer = PKCS1_v1_5.new(RSA.importKey(binascii.unhexlify(self.private_key)))
+        h = SHA256.new((str(sender) + str(recipient) + str(amount)).encode('utf'))
+        signature = signer.sign()
+        return binascii.hexlify(signature).decode('ascii')
