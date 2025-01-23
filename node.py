ed717aac2965eb3461830b2164a74ed31b57a381
Packo0 <pdimitrov1389@gmail.com>
2025-01-23 11:48:59 +0200

    setting up a routes for managing the wallet

--- node.py
+++ node.py
@@ -10,6 +10,34 @@ blockchain = Blockchain(wallet.public_key)
 CORS(app)
 
 
+@app.route("/wallet", methods=["POST"])
+def create_keys():
+    wallet.create_keys()
+    if wallet.save_keys():
+        response = {"public_key": wallet.public_key, "private_key": wallet.private_key}
+
+        global blockchain
+        blockchain = Blockchain(wallet.public_key)
+        return jsonify(response), 201
+    else:
+        response = {"message": "Saving the keys failed."}
+        return jsonify(response), 500
+
+
+@app.route("/wallet", methods=["GET"])
+def load_keys():
+    wallet.load_keys()
+    if wallet.load_keys():
+        response = {"public_key": wallet.public_key, "private_key": wallet.private_key}
+
+        global blockchain
+        blockchain = Blockchain(wallet.public_key)
+        return jsonify(response), 201
+    else:
+        response = {"message": "Loading the keys failed."}
+        return jsonify(response), 500
+
+
 @app.route("/", methods=["GET"])
 def get_ui():
     return "This works!"
@@ -21,10 +49,7 @@ def mine():
     if block != None:
         dict_block = block.__dict__.copy()
         dict_block["transactions"] = [tx.__dict__ for tx in dict_block["transactions"]]
-        response = {
-            "message": "Block added successfully.",
-            'block': dict_block
-        }
+        response = {"message": "Block added successfully.", "block": dict_block}
 
         return jsonify(response), 201
     else:
